756737fdc90dd0db08ffbf12d2a8c6c9abf0481c
PhiAbs <ph.andermatt@gmail.com>
2019-05-29 09:25:22 +0000

    changed files for run 2b. since training became very slow, we also set up the resume_training.sh file to continue training with an intermediate weight file

--- training_files/resume_training.sh
+++ training_files/resume_training.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
-
-./build/tools/caffe train \
-    --solver=models/bvlc_reference_caffenet/solver.prototxt \
-    --snapshot=models/bvlc_reference_caffenet/caffenet_train_10000.solverstate.h5 \
+./../../caffe/build/tools/caffe train \
+    --solver=../models/caffenet/solver.prototxt \
+    --snapshot=../models/caffenet/run2b_dog_cat_female_male_ball_continued/solver_iter_80000.solverstate \
+    2>&1 | tee /home/pussycat/finetune_caffe_model/logging/finetune_on_2b_dog_cat_female_male_ball_continued_02.log
     $@
